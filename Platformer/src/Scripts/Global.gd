5ec1f4cc5e468fbc7adfcb05bc1c22f3edcb087b
TheBeeEmperor <104157076+TheBeeEmperor@users.noreply.github.com>
2023-12-14 10:47:57 +0300

    Delete Platformer directory

--- Platformer/src/Scripts/Global.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var max_health = 100.0
-var health = max_health
--- Platformer/src/Scripts/Portal.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite2D
-
-@export var Scene : String
-
-func _on_area_2d_area_entered(area):
-	if area.is_in_group("Player"):
-		get_tree().change_scene_to_file(Scene)
